a1b105cf1c8b7446ce5512f3b38a5bbd1a1b41e4
vaul123 <w.chanski@gmail.com>
2017-02-08 15:14:29 +0100

    ajax

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/vaultdweller.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            var response = response;
+            var badges = $('#badges');
+            $.each(response.courses.completed, function (index,course){
+                var div = $('<div></div>').addClass('course');
+                var title = $('<h3>'+ course.title+'</h3>');
+                var badgeImg = course.badge;
+                var img = $("<img src="+badgeImg+">");
+                var done = div.append(title).append(img);
+                badges.append(done);
+            })
+
+        }
+    });
 
 });
